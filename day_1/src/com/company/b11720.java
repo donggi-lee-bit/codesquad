dc4ee6731c2358a31ed5223333700e7c85862158
donggi-lee-bit <af1505@naver.com>
2021-11-01 17:47:26 +0900

    please day 1

---
+++ day_1/src/com/company/b11720.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class b11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> num = new ArrayList<Integer>();
+
+    }
+}
