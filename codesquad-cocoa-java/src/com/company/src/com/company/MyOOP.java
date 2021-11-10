7bb7292c2294786e05783ea6ab8b3888f0305aa7
donggi-lee-bit <af1505@naver.com>
2021-11-10 14:46:04 +0900

    Instance
    알게된 것 :
    - 앞서 instance는 클래스의 복제본을 변수에 담은 것이라고 했다. 더 나아가
      이 instance를 사용하면 클래스의 어떠한 상태를 다양하게 사용할 수 있게
      된다.
    - 이때 기존에 사용했던 static을 지워줘야 instance로 사용이 가능하게
      된다.

--- codesquad-cocoa-java/src/com/company/src/com/company/MyOOP.java
+++ codesquad-cocoa-java/src/com/company/src/com/company/MyOOP.java
@@ -1,14 +1,14 @@
 package com.company;
 
 class  Print{
-  public static String delimiter = "";
-  public static void A() {
+  public String delimiter = "";
+  public void A() {
     System.out.println(delimiter);
     System.out.println("A");
     System.out.println("A");
   }
 
-  public static void B() {
+  public void B() {
     System.out.println(delimiter);
     System.out.println("B");
     System.out.println("B");
@@ -20,19 +20,27 @@ public class MyOOP {
 
   public static void main(String[] args) {
 
-    Print.delimiter = "----";
-    //..
-    Print.A();
-    Print.A();
-    Print.B();
-    Print.B();
-    //..
-    Print.delimiter = "****";
-    Print.A();
-    Print.A();
-    Print.B();
-    Print.B();
-    
+    Print p1 = new Print();
+    p1.delimiter = "----";
+    p1.A();
+    p1.A();
+    p1.B();
+    p1.B();
+
+    Print p2 = new Print();
+    p2.delimiter = "****";
+    p2.A();
+    p2.A();
+    p2.B();
+    p2.B();
+
+    p1.A();
+    p2.A();
+    p1.A();
+    p2.A();
+    p1.A();
+    p2.A();
+
     
   }
 }
