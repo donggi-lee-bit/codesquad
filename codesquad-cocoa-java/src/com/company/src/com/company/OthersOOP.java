773d187b6237df1b8ff9b962e8372f03e54fce84
donggi-lee-bit <af1505@naver.com>
2021-11-10 12:09:02 +0900

    생활코딩 객체지향 강의
    알게된 것 :
    - 클래스에 대해 명시적으로 더 알게되었다. System.out.print 할 때 System
      또한 클래스라는 걸 알았다.
    - instance 는 내가 생성한 변수(?)를 나타내는 것 같다. 아니면 new 를 통해
      만들어진 걸 instance라고 하는걸까?

---
+++ codesquad-cocoa-java/src/com/company/src/com/company/OthersOOP.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OthersOOP {
+
+  public static void main(String[] args) throws IOException {
+    // class : System, Math, FileWriter
+    // instance : f1, f2
+
+    // Math 라고 하는 클래스에, PI 라는 변수가 있다.
+    System.out.println(Math.PI);
+    // floor(바닥)로 소수점을 내려주는 Math의 method이다.
+    System.out.println(Math.floor(1.8));
+    // ceil(천장)로 소수점을 올려주는 Math의 method이다.
+    System.out.println(Math.ceil(1.8));
+
+    // FileWrite 클래스
+    FileWriter f1 = new FileWriter("data.txt");
+    f1.write("Hello");
+    f1.write(" Java");
+    f1.write(" Hello World!");
+    f1.write(" Hi, How are you ?");
+    f1.close();
+
+    FileWriter f2 = new FileWriter("data2.txt");
+    f2.write("Hello");
+    f2.write(" Java2");
+    f2.write(" Hello World!");
+    f2.write(" Hi, How are you ?");
+    f2.close();
+  }
+
+}
