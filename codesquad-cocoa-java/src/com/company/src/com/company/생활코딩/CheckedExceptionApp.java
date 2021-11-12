761ea1d25f8d7ef9b546f8313508ab4022c9467c
donggi-lee-bit <af1505@naver.com>
2021-11-12 16:04:49 +0900

    Checked Exception 이었던 FileWriter 클래스의 f.close()
    알게된 것 :
    - 기존에 txt 파일에 Hello를 쓰고 close를 바로 아래줄에서 해주었다.
      하지만 try가 실행되지 않을 수도 있기 때문에 close를 하기 전에 예외가
      발생할 때를 대비하여 finally로 처리해줘야했다.
    - try catch 문에 finally 문(?)이 있는데 그 곳에 다시 try catch 문을
      넣어줘서 IOException을 처리해주었다.
    - 글로 표현하는 게 좀 어렵다. 이해가 잘 안 됐나보다.

--- codesquad-cocoa-java/src/com/company/src/com/company/생활코딩/CheckedExceptionApp.java
+++ codesquad-cocoa-java/src/com/company/src/com/company/생활코딩/CheckedExceptionApp.java
@@ -6,13 +6,22 @@ import java.io.IOException;
 public class CheckedExceptionApp {
 
   public static void main(String[] args) {
+    FileWriter f = null;
     try {
-      FileWriter f = new FileWriter("data.txt");
+      f = new FileWriter("data.txt");
       f.write("Hello");
-      f.close();
+      // close를 하기 전에 예외가 발생할 수 있기 때문에 finally로 처리해야 한다.
+
     } catch (IOException e) {
       e.printStackTrace();
+    } finally {
+      if (f != null) {
+        try {
+          f.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
   }
-
 }
