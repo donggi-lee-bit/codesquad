5afdea893ccb8655ab577fe39b8904620d3fdda0
donggi-lee-bit <af1505@naver.com>
2021-11-22 17:41:09 +0900

    Label Method

---
+++ AWTSTUDY/src/com/donggi/LabelTest.java
@@ -0,0 +1,24 @@
+package com.donggi;
+
+import java.awt.Frame;
+import java.awt.Label;
+
+public class LabelTest {
+
+  public static void main(String[] args) {
+    Frame f = new Frame("Login");
+    f.setSize(300, 200);
+    f.setLayout(null);
+
+    Label id = new Label("ID:");
+    id.setBounds(50, 50, 30, 10); // 50, 50 위치에 크기가 가로 30, 세로 10
+
+    Label pwd = new Label("Password :");
+    pwd.setBounds(50, 65, 100, 10);
+
+    f.add(id);
+    f.add(pwd);
+    f.setVisible(true);
+  }
+
+}
