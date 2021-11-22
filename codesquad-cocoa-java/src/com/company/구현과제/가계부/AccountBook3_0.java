c88d43ea0a8b55826d1f8c87308a857d449a7633
donggi-lee-bit <af1505@naver.com>
2021-11-23 00:05:30 +0900

    각각의 구현 과제에 패키지 추가

---
+++ codesquad-cocoa-java/src/com/company/구현과제/가계부/AccountBook3_0.java
@@ -0,0 +1,9 @@
+package com.company.구현과제;
+
+public class AccountBook3_0 {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ codesquad-cocoa-java/src/com/company/구현과제/행맨/HangMan3_2.java
@@ -0,0 +1,9 @@
+package com.company.구현과제;
+
+public class HangMan3_2 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Hangman !!!!");
+  }
+
+}
