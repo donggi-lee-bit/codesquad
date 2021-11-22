2638b13a7077f613d1986c807d8981ef4e9c87e2
donggi-lee-bit <af1505@naver.com>
2021-11-22 11:26:43 +0900

    Awt 스터디 Frame 클래스를 이용하여 화면에 Frame 나타내보기

---
+++ AWTSTUDY/src/com/donggi/FrameTest.java
@@ -0,0 +1,13 @@
+package com.donggi;
+
+import java.awt.Frame;
+
+public class FrameTest {
+
+  public static void main(String[] args) {
+    Frame f = new Frame("Login");
+    f.setSize(300, 200);
+    f.setVisible(true); // 생성한 Frame 객체를 화면에 나타내는 메소드
+  }
+
+}
