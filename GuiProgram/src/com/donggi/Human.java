4c312d5320df9bd9778be45b03e576bd7c6a3047
donggi-lee-bit <devdonggilee@gmail.com>
2021-11-24 04:23:23 +0900

    오버라이드 예제 만들어봄
    - 자식 클래스의 메소드에 오버라이드를 해주니 부모 클래스에 있는 메소드를
      찾지 않고 자식 클래스의 메소드가 호출되는 것을 볼 수 있었다

---
+++ GuiProgram/src/com/donggi/Human.java
@@ -0,0 +1,16 @@
+package com.donggi;
+
+public class Human {
+
+  public Human() {
+    System.out.println("4. Human class 입니다 ");
+  }
+
+  public void method1() {
+    System.out.println("5. Human class의 method1입니다");
+  }
+
+  public void method2() {
+    System.out.println("6. Human class의 method2입니다");
+  }
+}
--- GuiProgram/src/com/donggi/Main.java
+++ GuiProgram/src/com/donggi/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Student student = new Student();
+        student.method1();
+        student.method2();
+        student.method3();
     }
 }
