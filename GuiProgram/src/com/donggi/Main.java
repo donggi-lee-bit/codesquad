f49c437bffdc7cb46d0cc7746f77bd084ba4d61c
donggi-lee-bit <devdonggilee@gmail.com>
2021-11-24 20:48:32 +0900

    TextArea 메소드 사용하니 프레임에 본문 적는 칸이 생겼다.

--- GuiProgram/src/com/donggi/Main.java
+++ GuiProgram/src/com/donggi/Main.java
@@ -1,12 +1,2 @@
-package com.donggi;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        Student student = new Student();
-        student.method1();
-        student.method2();
-        student.method3();
-    }
+package com.donggi;public class Main {
 }
--- GuiProgram/src/com/donggi/Monster.java
+++
@@ -1,2 +0,0 @@
-package com.donggi;public class Monster {
-}
