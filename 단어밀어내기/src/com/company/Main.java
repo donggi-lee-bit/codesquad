155e8e112b7c765b885fff21fbdf4305a763ebc1
donggi-lee-bit <devdonggilee@gmail.com>
2021-11-30 23:27:05 +0900

    word exception
    - nextLine으로 받아서 단어가 하나가 아닐 경우를 checkWord()로 확인하는
      기능을 만들었다
    - 입력된 문자가 띄어쓰기를 기준으로 split을 하는데 split한 문자를 담고
      있는 변수의 길이가 1보다 클 때 오류 알림을 출력하고 다시 입력으로
      돌아가게 한다
    - 근데 그다음번에 올바른 출력을 하면 이전의 잘못 입력된 값이 계속
      저장되어있다가 같이 나온다 ㅜ

---
+++ 단어밀어내기/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.pushouttheword.Input;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        start();
+    }
+
+    public static void start() {
+        Input input = new Input();
+        input.userInput();
+    }
+}
--- 단어밀어내기/src/com/company/pushouttheword/Input.java
+++ 단어밀어내기/src/com/company/pushouttheword/Input.java
@@ -9,7 +9,6 @@ public class Input {
     Machine machine = new Machine();
 
     public void userInput() {
-        // trim() 으로 공백 지워야겠다
         System.out.print("하나의 단어를 입력해주세요 : ");
         String inputWord = sc.nextLine();
 
@@ -19,46 +18,44 @@ public class Input {
         System.out.print("단어를 이동시키고자 하는 방향을 입력해주세요 : ");
         String inputDir = sc.next().toUpperCase();
 
-
-//        check(inputWord, inputCount, inputDir);
+        check(inputWord, inputCount, inputDir);
 
         machine.game(inputWord, inputCount, inputDir);
     }
 
-//    private void check(String word, int count, String dir) {
-//
-//        checkWord(word);
-//        checkCount(count);
-//        checkDir(dir);
-//    }
+    private void check(String word, int count, String dir) {
 
-//    private String checkWord(String word) {
-//        if (word.split(" ").length == 1) {
-//            return word;
-//        } else {
-//            System.out.println("잘못된 형식의 입력입니다.");
-//            Main.start();
-//            return word;
-//        }
-//    }
+        checkWord(word);
+        checkCount(count);
+        checkDir(dir);
+    }
 
-//    private int checkCount(int count) {
-//        if (-100 <= count && count < 100) {
-//            return count;
-//        } else {
-//            System.out.println("잘못된 형식의 입력입니다.");
-//            Main.start();
-//            return count;
-//        }
-//    }
-//
-//    private String checkDir(String dir) {
-//        if (dir.equals("r") || dir.equals("R") || dir.equals("l") || dir.equals("L")) {
-//            return dir;
-//        } else {
-//            System.out.println("잘못된 형식의 입력입니다.");
-//            Main.start();
-//            return dir;
-//        }
-//    }
+    private void checkWord(String word) {
+        String[] wordArray = word.split(" ");
+
+        if (wordArray.length > 1) {
+            System.out.println("잘못된 형식의 입력입니다. \n");
+            Main.start();
+        }
+    }
+
+    private int checkCount(int count) {
+        if (-100 <= count && count < 100) {
+            return count;
+        } else {
+            System.out.println("잘못된 형식의 입력입니다.");
+            Main.start();
+            return count;
+        }
+    }
+
+    private String checkDir(String dir) {
+        if (dir.equals("r") || dir.equals("R") || dir.equals("l") || dir.equals("L")) {
+            return dir;
+        } else {
+            System.out.println("잘못된 형식의 입력입니다.");
+            Main.start();
+            return dir;
+        }
+    }
 }
