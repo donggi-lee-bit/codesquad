8f8e908c3d734e391e57a07e773ed2b296e2acb5
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-03 04:40:28 +0900

    LinkedList 를 이용해서 큐브 보드를 출력해보았다
    - LinkedList에 엘리먼트 넣는 게 조금 하드코딩 같지만 일단
      출력까지해보았다 ㅎㅎ

--- week5/FlatCube/src/com/donggi/Board.java
+++ week5/FlatCube/src/com/donggi/Board.java
@@ -1,10 +1,43 @@
 package com.donggi;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Board {
     public static void main(String[] args) {
-        Board board = new Board();
+//        Board board = new Board();
+//
+//        board.myBoard();
 
-        board.myBoard();
+        List<LinkedList<String>> listArray = new LinkedList<LinkedList<String>>();
+        List<String> list1 = new LinkedList<String>();
+        List<String> list2 = new LinkedList<>();
+        List<String> list3 = new LinkedList<>();
+
+        list1.add("R ");
+        list1.add("R ");
+        list1.add("W ");
+
+        listArray.add((LinkedList<String>) list1);
+
+        list2.add("G ");
+        list2.add("C ");
+        list2.add("W ");
+
+        listArray.add((LinkedList<String>) list2);
+
+        list3.add("G ");
+        list3.add("B ");
+        list3.add("B ");
+
+        listArray.add((LinkedList<String>) list3);
+
+        for (int i = 0; i < listArray.size(); i++) {
+            for (int j = 0; j < listArray.get(i).size(); j++) {
+                System.out.print(listArray.get(i).get(j));
+            }
+            System.out.println();
+        }
 
     }
 
@@ -17,6 +50,9 @@ public class Board {
                 {"G ", "C ", "W "},
                 {"G ", "B ", "B "}
         };
+
+
+
 
         // 오른쪽으로 한 칸 밀기
         String[][] tempArray = new String[1][1];
