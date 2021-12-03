5de66195ae16d95c4a56bcb7a3519d0e2fbb2e03
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-03 20:30:16 +0900

    평면 큐브 다시 시작 입력 기능 구현
    - 클래스 나누는 걸 생각하다보니 기본 구현도 안되어서 처음부탁 시작한다

---
+++ week5/FlatCube/src/com/donggi/Cube.java
@@ -0,0 +1,20 @@
+package com.donggi;
+
+import java.util.Scanner;
+
+public class Cube {
+
+    Cube() {
+        printCube();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("CUBE> ");
+        String[] command = sc.nextLine().split(" ");
+        mix();
+    }
+
+    private void mix() {
+    }
+
+    private void printCube() {
+    }
+}
--- week5/FlatCube/src/com/donggi/Main.java
+++ week5/FlatCube/src/com/donggi/Main.java
@@ -3,14 +3,6 @@ package com.donggi;
 public class Main {
 
     public static void main(String[] args) {
-
-        while (Command.isGoing) {
-            try {
-                System.out.print("CUBE> ");
-                new Command();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        new Cube();
     }
 }
