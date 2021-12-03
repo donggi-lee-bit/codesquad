07cb86d768092bdfb4aa9d05ebfd67b736a83363
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-03 20:45:37 +0900

    LinkedList 2차원 배열로 큐브 출력
    - row 만드는 메서드를 알게 되어서 적용해보았다

--- week5/FlatCube/src/com/donggi/Cube.java
+++ week5/FlatCube/src/com/donggi/Cube.java
@@ -1,8 +1,31 @@
 package com.donggi;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Cube {
+
+    List<LinkedList<String>> cubeBoard = new LinkedList<>();
+
+    void currentCube() {
+        cubeBoard.add(new LinkedList<>());
+        cubeBoard.get(0).add("R ");
+        cubeBoard.get(0).add("R ");
+        cubeBoard.get(0).add("W ");
+
+        cubeBoard.add(new LinkedList<>());
+        cubeBoard.get(1).add("G ");
+        cubeBoard.get(1).add("C ");
+        cubeBoard.get(1).add("W ");
+
+        cubeBoard.add(new LinkedList<>());
+        cubeBoard.get(2).add("G ");
+        cubeBoard.get(2).add("B ");
+        cubeBoard.get(2).add("B ");
+
+    }
 
     Cube() {
         printCube();
@@ -16,5 +39,15 @@ public class Cube {
     }
 
     private void printCube() {
+        currentCube();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cubeBoard.size(); i++) {
+            for (int j = 0; j < cubeBoard.get(i).size(); j++) {
+                sb.append(cubeBoard.get(i).get(j));
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
