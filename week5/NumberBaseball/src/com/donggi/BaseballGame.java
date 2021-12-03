7941671a30d0f36296888a693a833557a7519031
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-04 01:51:00 +0900

    컴퓨터 숫자와 입력 숫자 비교
    - 입력 받은 숫자는 String으로 받았기 때문에 parseInt로 정수로 바꿔줬다
    - 2중 for loop로 숫자 비교하게 하였다

--- week5/NumberBaseball/src/com/donggi/BaseballGame.java
+++ week5/NumberBaseball/src/com/donggi/BaseballGame.java
@@ -7,14 +7,27 @@ public class BaseballGame {
     Scanner sc = new Scanner(System.in);
     String[] numberArray;
     int[] computerNumber = new int[3];
+    int[] realNumberArray = new int[3];
+    int ball;
+    int strike;
 
     BaseballGame() {
         ComputerNumber();
 
         System.out.print("세 개의 숫자를 입력해주세요 : ");
         numberArray = sc.nextLine().split(" |");
-        System.out.println(numberArray[0]);
-        System.out.println(numberArray[1]);
+
+        for (int i = 0; i < numberArray.length; i++) {
+            realNumberArray[i] = Integer.parseInt(numberArray[i]);
+        }
+
+        for (int i = 0; i < computerNumber.length; i++) {
+            for (int j = 0; j < realNumberArray.length; j++) {
+                Integer.compare(computerNumber[i], realNumberArray[j]);
+                System.out.printf("compare 몇번하나 보자 ㅡㅡ i : %d j : %d \n", computerNumber[i], realNumberArray[j]);
+            }
+        }
+
 
     }
 
